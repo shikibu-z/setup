cd6f2ab0269bb7c308de8ed2c31985cf62ae246c
Junyong Zhao <junyong@seas.upenn.edu>
2021-08-17 16:12:29 -0400

    install-software: update virtualbox

--- install-software.sh
+++ install-software.sh
@@ -40,22 +40,31 @@ sudo apt install texlive-full
 
 # gui softwares
 sudo apt install baidunetdisk easytag libreoffice vlc qbittorrent vim-gtk3
+
 # google chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
+
 # skype
 wget https://repo.skype.com/latest/skypeforlinux-64.deb
 sudo apt install ./skypeforlinux-64.deb
+
 # zoom
 wget https://zoom.us/client/latest/zoom_amd64.deb
 sudo apt install ./zoom_amd64.deb
+
 # vscode
 curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update && sudo apt install code
 
-# virtualization
-sudo apt install virtualbox
+# virualbox
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib"
+sudo apt update && sudo apt install virtualbox-6.1
+
+# vagrant
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo add-apt-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt update && sudo apt install vagrant
