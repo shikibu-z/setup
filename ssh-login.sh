95495e78432c1d7e97525385c710847d57c7c6e5
Junyong Zhao <junyong@arizona.edu>
2022-08-29 10:46:09 -0700

    ssh: update ssh login options

--- ssh-login.sh
+++ ssh-login.sh
@@ -3,14 +3,13 @@
 ###
  # @Description : This is my shell script for ssh connections.
  # @Author      : Junyong Zhao (junyong@arizona.edu)
- # @LastEditTime: 2021-07-13 10:03:15
-### 
+ # @LastEditTime: 2022-08-29 10:43:46
+###
 
 host="$1"
 
 case "$host" in
-    "halloween") ssh junyong@158.130.62.116 ;;
-    "seasnet") ssh junyong@158.130.62.105 ;;
+    "lectura") sshpass -p QDCznZewEX ssh junyong@lectura.cs.arizona.edu ;;
     *) printf "Invalid host name: %s\n" "$host" >&2
         exit 1 ;;
 esac
